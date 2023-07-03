2c04d95b05b22be5d4be76541ef5a7c6e672261c
Aravin <4869265+Aravin@users.noreply.github.com>
2023-07-03 17:05:15 +0530

    updated hardcoded app config

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -4,7 +4,7 @@ dotenv.config();
 export const appConfig = {
     port: process.env.PORT || 3000,
     apiKey: process.env.API_KEY,
-    basePath: process.env.FINVASIA_BASEPATH || 'https://shoonyatrade.finvasia.com/NorenWClientTP',
+    basePath: process.env.FINVASIA_BASEPATH || 'https://api.shoonya.com/NorenWClientTP',
     apkVersion: process.env.FINVASIA_APK_VERSION || 'js:1.0.0',
     source: process.env.FINVASIA_SOURCE || 'API',
 }
